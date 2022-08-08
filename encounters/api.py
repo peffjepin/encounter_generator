5a95162a53a04dd7c1b3f0c4728628c7bd5d6746
peffjepin <peffjepin@gmail.com>
2022-08-08 02:24:43 -0400

    updated encounters module to now return Monster types instead of names

--- encounters/api.py
+++ encounters/api.py
@@ -6,20 +6,20 @@ from .data.model import Monster
 
 datadir = pathlib.Path(__file__).parent / "data"
 monsters = [eval(ln) for ln in (datadir / "reprs").read_text().splitlines()]
-name_by_id = {m.id: m for m in monsters}
-id_by_name = {m.name.lower(): m.id for m in monsters}
+monsters_by_id = {m.id: m for m in monsters}
+monsters_by_name = {m.name: m for m in monsters}
 
 
 def generate(player_levels, difficulty, seed=None, seed_monsters=None):
     if seed is None:
         seed = int(time.time() * 10000000)
     ids = _encounters.generate(player_levels, difficulty, seed, seed_monsters)
-    return tuple(name_by_id.get(id).name for id in ids)
+    return tuple(monsters_by_id[id] for id in ids)
 
 
 __all__ = (
     "monsters",
-    "name_by_id",
-    "id_by_name",
+    "monsters_by_id",
+    "monsters_by_name",
     "generate",
 )
--- scripts/test_python_module.py
+++ scripts/test_python_module.py
@@ -5,12 +5,20 @@ import encounters
 
 def main():
     for _ in range(20):
-        print(encounters.generate((1, 2, 3), 3))
+        print("----------------------------------")
+        for monster in encounters.generate((3, 3, 3), 3):
+            print(monster.name)
+        print("----------------------------------")
 
+    print("==================================")
     print("using seed monsters")
+    print("==================================")
     for n in range(5):
-        seed_ids = [0] * (n + 1)
-        print(encounters.generate((1, 2, 3), 3, seed_monsters=seed_ids))
+        print("----------------------------------")
+        seed_monster_ids = [0] * (n+1)
+        for monster in encounters.generate((1, 2, 3), 3, seed_monsters=seed_monster_ids):
+            print(monster.name)
+        print("----------------------------------")
 
     return 0
 
